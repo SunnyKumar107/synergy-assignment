cee8090f030158fc017d3d93a70e44ffca275778
Sunny Kumar <goodluck14115@gmail.com>
2024-10-05 16:08:37 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-red-500 text-2xl'>Synergy Labs | Assignment</h1>
+      <Button>Click here</Button>
+    </div>
+  )
+}
+
+export default App
